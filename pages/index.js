dc9c7813d8e1df2cd9c063c49c28b997ce2b4c71
IanPOS <contato.ianpires@outlook.com>
2026-01-06 20:34:53 -0300

    Challenge completed: curso.dev ep. 3/4

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,10 @@
-const Home = () => <h1>Home</h1>
+const Home = () => {
+    return (
+        <p>
+            💖 Espero de coração que você se cure de tudo o que te fez mal.
+            <br/>
+            Você é uma mulher incrível.
+        </p>
+    )
+}
 export default Home;
